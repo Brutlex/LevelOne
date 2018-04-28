b30dada14d847cecd6605007d4d7b97754006260
Bojan Miljevic <bojan.miljevic95@gmail.com>
2018-04-28 13:09:45 +0200

    Trigger collider

---
+++ LevelOne/Assets/Scripts/TriggerTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerTest : MonoBehaviour {
+
+    private bool triggered = false;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Contact was made!");
+        triggered = true;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("Contact!");
+        triggered = true;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("Contact broken!");
+        triggered = false;
+
+    }
+    void Update()
+ {
+        if (triggered == true)
+            {
+            gameObject.GetComponent<Renderer>().material.color = new Color(1, 0, 0, 1); //red
+        }
+        else
+        {
+            gameObject.GetComponent<Renderer>().material.color = new Color(0, 0, 0, 1); //black
+
+        }
+    }
+ }
+
