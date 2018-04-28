0988c9fbc3620c76130402878b641e0253301136
monika-brodbeck <monika.brodbeck@web.de>
2018-04-28 10:46:02 +0200

    improve player movement

--- LevelOne/Assets/Scripts/PlayerController.cs
+++ LevelOne/Assets/Scripts/PlayerController.cs
@@ -2,26 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
     public float speed = 2.0f;
+    public bool continousMovement = false;
 
-	void Update () {
-		if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+    void Update()
+    {
+        if (continousMovement)
         {
-            transform.Translate(-Vector2.right * speed);
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.Translate(-Vector2.right * speed);
+            }
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+            {
+                transform.Translate(Vector2.up * speed);
+            }
+            if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            {
+                transform.Translate(-Vector2.up * speed);
+            }
+            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.Translate(Vector2.right * speed);
+            }
         }
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        else
         {
-            transform.Translate(Vector2.up * speed);
+            if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                transform.Translate(-speed, 0, 0);
+
+            }
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                transform.Translate(0, speed, 0);
+            }
+            if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                transform.Translate(0, -speed, 0);
+
+            }
+            if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                transform.Translate(speed, 0, 0);
+            }
         }
-        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
-        {
-            transform.Translate(-Vector2.up * speed);
-        }
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.Translate(Vector2.right * speed);
-        }
+
     }
 }
