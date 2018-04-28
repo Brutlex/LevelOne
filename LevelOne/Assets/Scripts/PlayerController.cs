c5394db54edcfc465532574ea2f6c4711408ecea
Bojan Miljevic <bojan.miljevic95@gmail.com>
2018-04-28 09:19:30 +0200

    Simple Movement

--- LevelOne/Assets/Scripts/PlayerController.cs
+++ LevelOne/Assets/Scripts/PlayerController.cs
@@ -6,13 +6,22 @@ public class PlayerController : MonoBehaviour {
 
     public float speed = 1.0f;
 
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
 	void Update () {
-		
-	}
+		if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-Vector2.right * speed);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector2.up * speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(-Vector2.up * speed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * speed);
+        }
+    }
 }
