85503586fe5e5cc245108de18d77d43bbee1334b
monika-brodbeck <monika.brodbeck@web.de>
2018-04-28 10:06:35 +0200

    add player movement

--- LevelOne/Assets/Scripts/PlayerController.cs
+++ LevelOne/Assets/Scripts/PlayerController.cs
@@ -13,6 +13,23 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            gameObject.transform.Translate(0, -2, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            gameObject.transform.Translate(0, 2, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            gameObject.transform.Translate(2, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            gameObject.transform.Translate(-2, 0, 0);
+        }
+    }
 }
