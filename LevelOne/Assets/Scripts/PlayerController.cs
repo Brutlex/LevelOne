c6475540931aae9cf0b65aa70e1662d922799330
monika-brodbeck <monika.brodbeck@web.de>
2018-04-28 10:14:12 +0200

    update player speed and add arrow buttons

--- LevelOne/Assets/Scripts/PlayerController.cs
+++ LevelOne/Assets/Scripts/PlayerController.cs
@@ -4,22 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    public float speed = 1.0f;
+    public float speed = 2.0f;
 
 	void Update () {
-		if (Input.GetKey(KeyCode.A))
+		if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             transform.Translate(-Vector2.right * speed);
         }
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
             transform.Translate(Vector2.up * speed);
         }
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
             transform.Translate(-Vector2.up * speed);
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             transform.Translate(Vector2.right * speed);
         }
