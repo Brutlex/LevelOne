e92dbee2775b6a93dafd9f398ea8de971471d905
Bojan Miljevic <bojan.miljevic95@gmail.com>
2018-04-28 18:40:57 +0200

    Menu Prototype

---
+++ LevelOne/Assets/Scripts/LoadScreenOnClick.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScreenOnClick : MonoBehaviour {
+
+    public void LoadByIndex(int sceneindex)
+    {
+        SceneManager.LoadScene(sceneindex);
+    }
+
+    public void LoadByname(string scenename)
+    {
+        SceneManager.LoadScene(scenename);
+    }
+
+}
